0a32ec11760b9a6be80b2c9ac1b1bde03661a9d6
Sergey Opasen <ykcyc13@gmail.com>
2023-09-20 14:41:24 +0200

    hw-02-step-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
-import { Feedback } from './Feedback/Feedback';
-export const App = () => {
+import React, { useState } from 'react';
+import Feedback from './Feedback/Feedback';
+
+export function App() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = value => {
+    setState(prevState => ({
+      ...prevState,
+      [value]: prevState[value] + 1,
+    }));
+  };
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
-      <Feedback></Feedback>
+      <h1>Leave your feedback, please</h1>
+      <Feedback state={state} handleFeedback={handleFeedback} />
     </div>
   );
-};
+}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,46 +1,27 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
+import React from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
 
-export function Feedback() {
-  const [state, setState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+const Feedback = ({ state, handleFeedback }) => (
+  <>
+    <FeedbackOptions
+      options={Object.keys(state)}
+      onLeaveFeedback={handleFeedback}
+    />
+    <Statistics
+      good={state.good}
+      neutral={state.neutral}
+      bad={state.bad}
+      total={state.good + state.neutral + state.bad}
+      positivePercentage={
+        state.good > 0
+          ? Math.round(
+              (state.good / (state.good + state.neutral + state.bad)) * 100
+            )
+          : 0
+      }
+    />
+  </>
+);
 
-  const handleFeedback = type => {
-    setState(prevState => ({
-      ...prevState,
-      [type]: prevState[type] + 1,
-    }));
-  };
-  const countTotalFeedback = () => {
-    return state.good + state.neutral + state.bad;
-  };
-  const countPositiveFeedbackPercentage = () => {
-    const total = countTotalFeedback();
-    return total > 0 ? Math.round((state.good / total) * 100) : 0;
-  };
-  return (
-    <div>
-      <h1>Leave your feedback, please</h1>
-      <div style={{ display: 'flex', gap: '20px', margin: '0, auto' }}>
-        <Button variant="contained" onClick={() => handleFeedback('good')}>
-          Good
-        </Button>
-        <Button variant="contained" onClick={() => handleFeedback('neutral')}>
-          Neutral
-        </Button>
-        <Button variant="contained" onClick={() => handleFeedback('bad')}>
-          Bad
-        </Button>
-      </div>
-      <h2>Statistics</h2>
-      <p>Good: {state.good}</p>
-      <p>Neutral: {state.neutral}</p>
-      <p>Bad: {state.bad}</p>
-      <p>Total: {countTotalFeedback()}</p>
-      <p>Positive: {countPositiveFeedbackPercentage()}%</p>
-    </div>
-  );
-}
+export default Feedback;
