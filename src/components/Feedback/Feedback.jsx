487ac918a610b695ff3aaca96cd4686673696dda
Sergey Opasen <ykcyc13@gmail.com>
2023-09-20 15:02:09 +0200

    hw-02-step-4

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
+import Notification from './Notification';
 
-const Feedback = ({ state, handleFeedback }) => (
-  <>
-    <FeedbackOptions
-      options={Object.keys(state)}
-      onLeaveFeedback={handleFeedback}
-    />
-    <Statistics
-      good={state.good}
-      neutral={state.neutral}
-      bad={state.bad}
-      total={state.good + state.neutral + state.bad}
-      positivePercentage={
-        state.good > 0
-          ? Math.round(
-              (state.good / (state.good + state.neutral + state.bad)) * 100
-            )
-          : 0
-      }
-    />
-  </>
-);
+const Feedback = ({ state, handleFeedback }) => {
+  const { good, neutral, bad } = state;
+  const totalFeedback = good + neutral + bad;
+  return (
+    <>
+      <FeedbackOptions
+        options={Object.keys(state)}
+        onLeaveFeedback={handleFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Statistics
+          good={state.good}
+          neutral={state.neutral}
+          bad={state.bad}
+          total={state.good + state.neutral + state.bad}
+          positivePercentage={
+            state.good > 0
+              ? Math.round(
+                  (state.good / (state.good + state.neutral + state.bad)) * 100
+                )
+              : 0
+          }
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </>
+  );
+};
 
 export default Feedback;
